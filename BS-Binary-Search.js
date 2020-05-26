ae67ec7a2214007376504d8693e9ad45ed721011
Francisco Ayala <franciscoa@bluetrailsoft.com>
2020-05-26 14:56:14 -0500

    Update and fix comments on Binary Search

--- BS-Binary-Search.js
+++ BS-Binary-Search.js
@@ -7,7 +7,11 @@
 *   we have two important elements on this kind of search, min and max are the pivots that let us know
 *   our limits, and they gonna change each attempt, and they gonna start on 0 (first position) and n-1 (last position)
 *
-*   the biggest number of attempts that we can have with this search to find our element is 9! 
+*   the biggest number of attempts that we can have with this search to find our element is log n (+1)! 
+*   log2n = how many times we need to divide in 2 our current value to achieve the 1.
+*
+*   example: For the Tycho-2 star catalog with 2,539,913 stars, the nearest power of 2 below is 2 21 twenty-one
+*   2, start superscript, 21, end superscript (which is 2,097,152), so at most we would need 22 tries. Much better than a linear search!
 */
 
 const data = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
